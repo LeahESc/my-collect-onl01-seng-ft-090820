6e2de3ee6da498096b45fb351539c8ad6a4ea916
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 18:26:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
+ collection = []
  i = 0 
  while i < array.length 
- new_collection << yield(array[i])
+ collection << yield(array[i])
  i += 1 
  end
- new_collection
+ collection.join("")
 end 
