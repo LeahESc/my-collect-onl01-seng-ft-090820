1cc634f7a7047348b79dc24ffa250e947f9e0627
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 18:22:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+ i = 0 
+ while i < array.length 
+ yield(array[i])
+ end
+end 
 
-
+def my_collect(array)
+ i = 0 
+ while i < array.length 
+ collection << yield(array[i])
+ end
+ collection
+end 
